e9872e9e88877ade634223f4caca352c1ed04b05
Peter Muys <peter@petermuys.com>
2016-09-15 22:14:50 +0200

    WIP ROD Compiler - Value Class Code Generator

--- src/main/java/com/persistentbit/robjects/javagen/ServiceJavaGen.java
+++ src/main/java/com/persistentbit/robjects/javagen/ServiceJavaGen.java
@@ -64,6 +64,7 @@ public class ServiceJavaGen {
             imports.filter(i -> i.packageName.equals(servicePackageName) == false).forEach(i -> {
                 sg.println("import " + i.packageName + "." + i.className + ";");
             });
+            sg.println("");
             sg.add(this);
             return new GeneratedJava(cls,sg.writeToString());
         }
@@ -88,6 +89,7 @@ public class ServiceJavaGen {
                     println(toString(p.valueType) + " " + p.name + ";");
                 });
                 println("");
+                //***** MAIN CONSTRUCTOR
                 bs("public " + vc.typeSig.name.className + "(" +
                         vc.properties.map(p -> toString(p.valueType.typeSig) + " " + p.name ).toString(", ")
                         +")");{
@@ -98,25 +100,27 @@ public class ServiceJavaGen {
                             fromValue = "Objects.requireNonNull(" + p.name + ",\"" + p.name  + " in " + vc.typeSig.name.className + " can\'t be null\")";
                         }
                         else {
-
-                            fromValue = "Optional.ofNullable(" + fromValue + ")";
+                            if(options.generateGetters == false) {
+                                fromValue = "Optional.ofNullable(" + fromValue + ")";
+                            }
                         }
                         println("this." + p.name + " = " + fromValue + ";");
                     });
                 }be();
+                //****** EXTRA CONSTRUCTORS FOR NULLABLE PROPERTIES
                 PList<RProperty> l = vc.properties;
                 PList<String> nullValues = PList.empty();
                 while(l.lastOpt().isPresent() && l.lastOpt().get().valueType.required == false){
                     l = l.dropLast();
                     nullValues = nullValues.plus("null");
-                    bs("public " + vc.typeSig.name + "(" +
+                    bs("public " + vc.typeSig.name.className + "(" +
                             l.map(p -> toString(p.valueType.typeSig) + " " + p.name ).toString(", ")
                             +")");{
                         println("this(" + l.map(p -> p.name).plusAll(nullValues).toString(",") + ");");
                     }be();
 
                 }
-
+                //****** GETTERS AND UPDATERS
                 vc.properties.forEach(p -> {
                     if(options.generateGetters){
                         String rt = toString(p.valueType.typeSig);
@@ -129,8 +133,16 @@ public class ServiceJavaGen {
                         println("public " + rt + " get" +firstUpper(p.name) + "() { return " + vn + "; }");
                     }
                     if(options.generateUpdaters){
-                        String s = "public " + vc.typeSig.name.className + "with" + firstUpper(p.name) + "( )";
-
+                        String s = "public " + toString(vc.typeSig) + " with" + firstUpper(p.name) + "("+ toString(p.valueType.typeSig) + " " + p.name +") { return new ";
+                        s += vc.typeSig.name.className;
+                        if(vc.typeSig.generics.isEmpty()){
+                            s += "<>";
+                        }
+                        s+= "(" + vc.properties.map(param -> {
+                            return (param.name.equals(p.name) ? "" : "this.") + param.name;
+                        }).toString(", ") + ")";
+                        s+= "; }";
+                        println(s);
                     }
                     println("");
                 });
