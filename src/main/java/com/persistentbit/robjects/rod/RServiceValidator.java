e0518358d0b076c6aacc69167be08c8a8df637ff
Peter Muys <peter@petermuys.com>
2016-09-17 16:00:05 +0200

    Added ROD verification for duplicated names/types or missing Types

--- src/main/java/com/persistentbit/robjects/rod/RServiceValidator.java
+++ src/main/java/com/persistentbit/robjects/rod/RServiceValidator.java
@@ -17,7 +17,56 @@ public class RServiceValidator {
 
     private void validate() {
         checkClassesDefined();
+        checkOverloading();
     }
+
+    private void checkOverloading() {
+        PList<RClass> dup =service.remoteClasses.map(rc -> rc.name)
+                .plusAll(service.valueClasses.map(vc->vc.typeSig.name))
+                .plusAll(service.enums.map(e -> e.name))
+                .duplicates();
+        if(dup.isEmpty() == false){
+            throw new RServiceException("Duplicated type definitions: " + dup.map(c -> c.packageName +"." + c.className).toString(", "));
+        }
+        service.remoteClasses.forEach(rc -> checkOverloading(rc));
+        service.valueClasses.forEach(vc -> checkOverloading(vc));
+        service.enums.forEach(e -> checkOverloading(e));
+    }
+    private void checkOverloading(RRemoteClass rc){
+        PList<String> dupFunNames = rc.functions.map(f -> f.name).duplicates();
+        PList<String> wrong = dupFunNames.filter(n -> rc.functions.filter(f -> f.name.equals(n)).map(f2-> f2.params.size()).duplicates().isEmpty() == false);
+        if(wrong.isEmpty() == false){
+            throw new RServiceException("Remote class " + rc.name.className + " has duplicated functions with the same parameter count: " + wrong.toString(", "));
+        }
+        rc.functions.forEach(f -> checkOverloading(rc,f));
+    }
+    private void checkOverloading(RRemoteClass rc,RFunction f){
+        PList<String> dup = f.params.map(p -> p.name).duplicates();
+        if(dup.isEmpty() == false){
+            throw new RServiceException("Remote class " + rc.name.className + " function " + f.name + " has duplicated parameters");
+        }
+    }
+    private void checkOverloading(REnum e){
+        PList<String> dup = e.values.duplicates();
+        if(dup.isEmpty() == false){
+            throw new RServiceException("enum " + e.name.className + " has duplicated values: " + dup.toString(", "));
+        }
+    }
+
+
+    private void checkOverloading(RValueClass vc){
+        PStream<String> dup = vc.typeSig.generics.map(sig -> sig.name.className).duplicates();
+        if(dup.isEmpty() == false){
+            throw new RServiceException("value class " + vc.typeSig.name.className + " has duplicated Generics parameters: " + dup.toString(", "));
+        }
+        dup = vc.properties.map(p->p.name).duplicates();
+        if(dup.isEmpty() == false){
+            throw new RServiceException("value class " + vc.typeSig.name.className + " has duplicated property names: " + dup.toString(", "));
+        }
+
+    }
+
+
 
     private void checkClassesDefined(){
         PSet<RClass>   needed   =   PSet.empty();
