0c5755eb672eed2d2f9896d2e028816564684ecf
Peter Muys <peter@petermuys.com>
2017-01-10 21:09:12 +0100

    :construction: wip

--- src/main/java/com/persistentbit/substema/RemoteServiceLogger.java
+++ src/main/java/com/persistentbit/substema/RemoteServiceLogger.java
@@ -1,19 +1,23 @@
 package com.persistentbit.substema;
 
-import com.persistentbit.core.logging.LogPrinter;
+import com.persistentbit.core.logging.entries.LogEntry;
 import com.persistentbit.core.result.Result;
+import com.persistentbit.core.tuples.Tuple2;
 
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 /**
  * @author Peter Muys
  * @since 22/09/2016
  */
 public class RemoteServiceLogger implements RemoteService{
-    private RemoteService   master;
+    private       RemoteService                     master;
+    private final Consumer<Tuple2<RCall, LogEntry>> effect;
 
-    public RemoteServiceLogger(RemoteService master) {
+    public RemoteServiceLogger(RemoteService master, Consumer<Tuple2<RCall, LogEntry>> effect) {
         this.master = master;
+        this.effect = effect;
     }
 
     @Override
@@ -21,7 +25,7 @@ public class RemoteServiceLogger implements RemoteService{
         return Result.function(call).code(l -> {
             return master.call(call)
                 .completed()
-                .withLogs(le -> LogPrinter.consoleInColor().print(le));
+                .withLogs(le -> effect.accept(Tuple2.of(call, le)));
         });
     }
 
